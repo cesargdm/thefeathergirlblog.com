304fea53b2edcc9bbe3c4d7a3cf4386807c8e028
cesargdm <cesarguadarrama@icloud.com>
2019-10-03 11:21:32 -0500

    Update code, remove ghost links and titles, enhace styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,25 +6,25 @@ const generateRSSFeed = require(`./src/utils/rss/generate-feed`)
 let ghostConfig
 
 try {
-    ghostConfig = require(`./.ghost`)
+  ghostConfig = require(`./.ghost`)
 } catch (e) {
-    ghostConfig = {
-        production: {
-            apiUrl: process.env.GHOST_API_URL,
-            contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-        },
-    }
+  ghostConfig = {
+    production: {
+      apiUrl: process.env.GHOST_API_URL,
+      contentApiKey: process.env.GHOST_CONTENT_API_KEY,
+    },
+  }
 } finally {
-    const { apiUrl, contentApiKey } =
-        process.env.NODE_ENV === `development`
-            ? ghostConfig.development
-            : ghostConfig.production
+  const { apiUrl, contentApiKey } =
+    process.env.NODE_ENV === `development`
+      ? ghostConfig.development
+      : ghostConfig.production
 
-    if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
-        throw new Error(
-            `GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`,
-        ) // eslint-disable-line
-    }
+  if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
+    throw new Error(
+      `GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`,
+    ) // eslint-disable-line
+  }
 }
 
 /**
@@ -35,52 +35,52 @@ try {
  *
  */
 module.exports = {
-    siteMetadata: {
-        siteUrl: config.siteUrl,
+  siteMetadata: {
+    siteUrl: config.siteUrl,
+  },
+  plugins: [
+    /**
+     *  Content Plugins
+     */
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.join(__dirname, `src`, `pages`),
+        name: `pages`,
+      },
     },
-    plugins: [
-        /**
-         *  Content Plugins
-         */
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: path.join(__dirname, `src`, `pages`),
-                name: `pages`,
-            },
-        },
-        // Setup for optimised images.
-        // See https://www.gatsbyjs.org/packages/gatsby-image/
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: path.join(__dirname, `src`, `images`),
-                name: `images`,
-            },
-        },
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-source-ghost`,
-            options:
-                process.env.NODE_ENV === `development`
-                    ? ghostConfig.development
-                    : ghostConfig.production,
-        },
-        /**
-         *  Utility Plugins
-         */
-        {
-            resolve: `gatsby-plugin-ghost-manifest`,
-            options: {
-                short_name: config.shortTitle,
-                start_url: `/`,
-                background_color: config.backgroundColor,
-                theme_color: config.themeColor,
-                display: `minimal-ui`,
-                icon: `static/${config.siteIcon}`,
-                legacy: true,
-                query: `
+    // Setup for optimised images.
+    // See https://www.gatsbyjs.org/packages/gatsby-image/
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.join(__dirname, `src`, `images`),
+        name: `images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-ghost`,
+      options:
+        process.env.NODE_ENV === `development`
+          ? ghostConfig.development
+          : ghostConfig.production,
+    },
+    /**
+     *  Utility Plugins
+     */
+    {
+      resolve: `gatsby-plugin-ghost-manifest`,
+      options: {
+        short_name: config.shortTitle,
+        start_url: `/`,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: `minimal-ui`,
+        icon: `static/${config.siteIcon}`,
+        legacy: true,
+        query: `
                 {
                     allGhostSettings {
                         edges {
@@ -92,12 +92,12 @@ module.exports = {
                     }
                 }
               `,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-feed`,
-            options: {
-                query: `
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
                 {
                     allGhostSettings {
                         edges {
@@ -109,13 +109,13 @@ module.exports = {
                     }
                 }
               `,
-                feeds: [generateRSSFeed(config)],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-advanced-sitemap`,
-            options: {
-                query: `
+        feeds: [generateRSSFeed(config)],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-advanced-sitemap`,
+      options: {
+        query: `
                 {
                     allGhostPost {
                         edges {
@@ -158,33 +158,33 @@ module.exports = {
                         }
                     }
                 }`,
-                mapping: {
-                    allGhostPost: {
-                        sitemap: `posts`,
-                    },
-                    allGhostTag: {
-                        sitemap: `tags`,
-                    },
-                    allGhostAuthor: {
-                        sitemap: `authors`,
-                    },
-                    allGhostPage: {
-                        sitemap: `pages`,
-                    },
-                },
-                exclude: [
-                    `/dev-404-page`,
-                    `/404`,
-                    `/404.html`,
-                    `/offline-plugin-app-shell-fallback`,
-                ],
-                createLinkInHead: true,
-                addUncaughtPages: true,
-            },
+        mapping: {
+          allGhostPost: {
+            sitemap: `posts`,
+          },
+          allGhostTag: {
+            sitemap: `tags`,
+          },
+          allGhostAuthor: {
+            sitemap: `authors`,
+          },
+          allGhostPage: {
+            sitemap: `pages`,
+          },
         },
-        `gatsby-plugin-catch-links`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-force-trailing-slashes`,
-        `gatsby-plugin-offline`,
-    ],
+        exclude: [
+          `/dev-404-page`,
+          `/404`,
+          `/404.html`,
+          `/offline-plugin-app-shell-fallback`,
+        ],
+        createLinkInHead: true,
+        addUncaughtPages: true,
+      },
+    },
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-force-trailing-slashes`,
+    `gatsby-plugin-offline`,
+  ],
 }
--- src/components/common/meta/ArticleMeta.js
+++ src/components/common/meta/ArticleMeta.js
@@ -12,109 +12,95 @@ import config from '../../../utils/siteConfig'
 import { tags as tagsHelper } from '@tryghost/helpers'
 
 const ArticleMetaGhost = ({ data, settings, canonical }) => {
-    const ghostPost = data
-    settings = settings.allGhostSettings.edges[0].node
+  const ghostPost = data
+  settings = settings.allGhostSettings.edges[0].node
 
-    const author = getAuthorProperties(ghostPost.primary_author)
-    const publicTags = _.map(
-        tagsHelper(ghostPost, { visibility: `public`, fn: (tag) => tag }),
-        `name`,
-    )
-    const primaryTag = publicTags[0] || ``
-    const shareImage = ghostPost.feature_image
-        ? ghostPost.feature_image
-        : _.get(settings, `cover_image`, null)
-    const publisherLogo =
-        settings.logo || config.siteIcon
-            ? url.resolve(config.siteUrl, settings.logo || config.siteIcon)
-            : null
+  const author = getAuthorProperties(ghostPost.primary_author)
+  const publicTags = _.map(
+    tagsHelper(ghostPost, { visibility: `public`, fn: (tag) => tag }),
+    `name`,
+  )
+  const primaryTag = publicTags[0] || ``
+  const shareImage = ghostPost.feature_image
+    ? ghostPost.feature_image
+    : _.get(settings, `cover_image`, null)
+  const publisherLogo =
+    settings.logo || config.siteIcon
+      ? url.resolve(config.siteUrl, settings.logo || config.siteIcon)
+      : null
 
-    return (
-        <>
-            <Helmet>
-                <title>{ghostPost.meta_title || ghostPost.title}</title>
-                <meta
-                    name="description"
-                    content={ghostPost.meta_description || ghostPost.excerpt}
-                />
-                <link rel="canonical" href={canonical} />
+  return (
+    <>
+      <Helmet>
+        <title>{ghostPost.meta_title || ghostPost.title}</title>
+        <meta
+          name="description"
+          content={ghostPost.meta_description || ghostPost.excerpt}
+        />
+        <link rel="canonical" href={canonical} />
 
-                <meta property="og:site_name" content={settings.title} />
-                <meta property="og:type" content="article" />
-                <meta
-                    property="og:title"
-                    content={
-                        ghostPost.og_title ||
-                        ghostPost.meta_title ||
-                        ghostPost.title
-                    }
-                />
-                <meta
-                    property="og:description"
-                    content={
-                        ghostPost.og_description ||
-                        ghostPost.excerpt ||
-                        ghostPost.meta_description
-                    }
-                />
-                <meta property="og:url" content={canonical} />
-                <meta
-                    property="article:published_time"
-                    content={ghostPost.published_at}
-                />
-                <meta
-                    property="article:modified_time"
-                    content={ghostPost.updated_at}
-                />
-                {publicTags.map((keyword, i) => (
-                    <meta property="article:tag" content={keyword} key={i} />
-                ))}
-                {author.facebookUrl && (
-                    <meta
-                        property="article:author"
-                        content={author.facebookUrl}
-                    />
-                )}
+        <meta property="og:site_name" content={settings.title} />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content={
+            ghostPost.og_title || ghostPost.meta_title || ghostPost.title
+          }
+        />
+        <meta
+          property="og:description"
+          content={
+            ghostPost.og_description ||
+            ghostPost.excerpt ||
+            ghostPost.meta_description
+          }
+        />
+        <meta property="og:url" content={canonical} />
+        <meta
+          property="article:published_time"
+          content={ghostPost.published_at}
+        />
+        <meta property="article:modified_time" content={ghostPost.updated_at} />
+        {publicTags.map((keyword, i) => (
+          <meta property="article:tag" content={keyword} key={i} />
+        ))}
+        {author.facebookUrl && (
+          <meta property="article:author" content={author.facebookUrl} />
+        )}
 
-                <meta
-                    name="twitter:title"
-                    content={
-                        ghostPost.twitter_title ||
-                        ghostPost.meta_title ||
-                        ghostPost.title
-                    }
-                />
-                <meta
-                    name="twitter:description"
-                    content={
-                        ghostPost.twitter_description ||
-                        ghostPost.excerpt ||
-                        ghostPost.meta_description
-                    }
-                />
-                <meta name="twitter:url" content={canonical} />
-                <meta name="twitter:label1" content="Written by" />
-                <meta name="twitter:data1" content={author.name} />
-                {primaryTag && (
-                    <meta name="twitter:label2" content="Filed under" />
-                )}
-                {primaryTag && (
-                    <meta name="twitter:data2" content={primaryTag} />
-                )}
+        <meta
+          name="twitter:title"
+          content={
+            ghostPost.twitter_title || ghostPost.meta_title || ghostPost.title
+          }
+        />
+        <meta
+          name="twitter:description"
+          content={
+            ghostPost.twitter_description ||
+            ghostPost.excerpt ||
+            ghostPost.meta_description
+          }
+        />
+        <meta name="twitter:url" content={canonical} />
+        <meta name="twitter:label1" content="Written by" />
+        <meta name="twitter:data1" content={author.name} />
+        {primaryTag && <meta name="twitter:label2" content="Filed under" />}
+        {primaryTag && <meta name="twitter:data2" content={primaryTag} />}
 
-                {settings.twitter && (
-                    <meta
-                        name="twitter:site"
-                        content={`https://twitter.com/${settings.twitter.replace(
-                            /^@/,
-                            ``,
-                        )}/`}
-                    />
-                )}
-                {settings.twitter && (
-                    <meta name="twitter:creator" content={settings.twitter} />
-                )}
-                <script type="application/ld+json">{`
+        {settings.twitter && (
+          <meta
+            name="twitter:site"
+            content={`https://twitter.com/${settings.twitter.replace(
+              /^@/,
+              ``,
+            )}/`}
+          />
+        )}
+        {settings.twitter && (
+          <meta name="twitter:creator" content={settings.twitter} />
+        )}
+        <script type="application/ld+json">{`
                     {
                         "@context": "https://schema.org/",
                         "@type": "Article",
@@ -122,37 +108,37 @@ const ArticleMetaGhost = ({ data, settings, canonical }) => {
                             "@type": "Person",
                             "name": "${author.name}",
                             ${
-                                author.image
-                                    ? author.sameAsArray
-                                        ? `"image": "${author.image}",`
-                                        : `"image": "${author.image}"`
-                                    : ``
+                              author.image
+                                ? author.sameAsArray
+                                  ? `"image": "${author.image}",`
+                                  : `"image": "${author.image}"`
+                                : ``
                             }
                             ${
-                                author.sameAsArray
-                                    ? `"sameAs": ${author.sameAsArray}`
-                                    : ``
+                              author.sameAsArray
+                                ? `"sameAs": ${author.sameAsArray}`
+                                : ``
                             }
                         },
                         ${
-                            publicTags.length
-                                ? `"keywords": "${_.join(publicTags, `, `)}",`
-                                : ``
+                          publicTags.length
+                            ? `"keywords": "${_.join(publicTags, `, `)}",`
+                            : ``
                         }
                         "headline": "${ghostPost.meta_title ||
-                            ghostPost.title}",
+                          ghostPost.title}",
                         "url": "${canonical}",
                         "datePublished": "${ghostPost.published_at}",
                         "dateModified": "${ghostPost.updated_at}",
                         ${
-                            shareImage
-                                ? `"image": {
+                          shareImage
+                            ? `"image": {
                                 "@type": "ImageObject",
                                 "url": "${shareImage}",
                                 "width": "${config.shareImageWidth}",
                                 "height": "${config.shareImageHeight}"
                             },`
-                                : ``
+                            : ``
                         }
                         "publisher": {
                             "@type": "Organization",
@@ -165,68 +151,68 @@ const ArticleMetaGhost = ({ data, settings, canonical }) => {
                             }
                         },
                         "description": "${ghostPost.meta_description ||
-                            ghostPost.excerpt}",
+                          ghostPost.excerpt}",
                         "mainEntityOfPage": {
                             "@type": "WebPage",
                             "@id": "${config.siteUrl}"
                         }
                     }
                 `}</script>
-            </Helmet>
-            <ImageMeta image={shareImage} />
-        </>
-    )
+      </Helmet>
+      <ImageMeta image={shareImage} />
+    </>
+  )
 }
 
 ArticleMetaGhost.propTypes = {
-    data: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        published_at: PropTypes.string.isRequired,
-        updated_at: PropTypes.string.isRequired,
-        meta_title: PropTypes.string,
-        meta_description: PropTypes.string,
-        primary_author: PropTypes.object.isRequired,
-        feature_image: PropTypes.string,
-        tags: PropTypes.arrayOf(
-            PropTypes.shape({
-                name: PropTypes.string,
-                slug: PropTypes.string,
-                visibility: PropTypes.string,
-            }),
-        ),
-        primaryTag: PropTypes.shape({
-            name: PropTypes.string,
-        }),
-        og_title: PropTypes.string,
-        og_description: PropTypes.string,
-        twitter_title: PropTypes.string,
-        twitter_description: PropTypes.string,
-        excerpt: PropTypes.string.isRequired,
-    }).isRequired,
-    settings: PropTypes.shape({
-        logo: PropTypes.object,
-        title: PropTypes.string,
-        twitter: PropTypes.string,
-        allGhostSettings: PropTypes.object.isRequired,
-    }).isRequired,
-    canonical: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    published_at: PropTypes.string.isRequired,
+    updated_at: PropTypes.string.isRequired,
+    meta_title: PropTypes.string,
+    meta_description: PropTypes.string,
+    primary_author: PropTypes.object.isRequired,
+    feature_image: PropTypes.string,
+    tags: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        slug: PropTypes.string,
+        visibility: PropTypes.string,
+      }),
+    ),
+    primaryTag: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+    og_title: PropTypes.string,
+    og_description: PropTypes.string,
+    twitter_title: PropTypes.string,
+    twitter_description: PropTypes.string,
+    excerpt: PropTypes.string.isRequired,
+  }).isRequired,
+  settings: PropTypes.shape({
+    logo: PropTypes.object,
+    title: PropTypes.string,
+    twitter: PropTypes.string,
+    allGhostSettings: PropTypes.object.isRequired,
+  }).isRequired,
+  canonical: PropTypes.string.isRequired,
 }
 
 const ArticleMetaQuery = (props) => (
-    <StaticQuery
-        query={graphql`
-            query GhostSettingsArticleMeta {
-                allGhostSettings {
-                    edges {
-                        node {
-                            ...GhostSettingsFields
-                        }
-                    }
-                }
+  <StaticQuery
+    query={graphql`
+      query GhostSettingsArticleMeta {
+        allGhostSettings {
+          edges {
+            node {
+              ...GhostSettingsFields
             }
-        `}
-        render={(data) => <ArticleMetaGhost settings={data} {...props} />}
-    />
+          }
+        }
+      }
+    `}
+    render={(data) => <ArticleMetaGhost settings={data} {...props} />}
+  />
 )
 
 export default ArticleMetaQuery
